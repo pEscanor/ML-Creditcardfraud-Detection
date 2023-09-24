289e345fea36879a5d071416bbdd64c310447072
Kofi <kofi_offei@icloud.com>
2023-09-24 08:06:01 +0200

    docs: add comments

--- model.py
+++ model.py
@@ -1,44 +1,62 @@
+# Import necessary libraries
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+# Define a function to check if a string represents an integer
 def RepresentsInt(s):
-    try: 
+    try:
         int(s)
         return True
     except ValueError:
         return False
 
-Y = []
-X = []
+# Initialize empty lists for target and feature data
+Y = []  # Target variable (fraud or not)
+X = []  # Feature variables
 
+# Open and read the CSV file
 with open("creditcard.csv", "r") as csvhandler:
-    data = csv.reader(csvhandler, delimiter=',');
+    data = csv.reader(csvhandler, delimiter=',')
     for row in data:
-        if (RepresentsInt(row[30])):
-            X.append([float(i) for i in row[1:30]]);
-            Y.append(float(row[30]));
-    
-X = np.array(X);
-Y = np.array(Y);
-    
-train_x, test_x, train_y, test_y = train_test_split(X,Y, test_size=0.5,random_state=20);
+        # Check if the last column (Class) represents an integer (1 for fraud, 0 for not)
+        if RepresentsInt(row[30]):
+            # Append feature values (columns 1 to 29) as floats to X
+            X.append([float(i) for i in row[1:30]])
+            # Append the target value (Class) as a float to Y
+            Y.append(float(row[30]))
 
-clf = LogisticRegression();
-clf.fit(train_x, train_y);
-print("Score:", clf.score(test_x, test_y));
+# Convert lists to NumPy arrays for further processing
+X = np.array(X)
+Y = np.array(Y)
 
-pred =  clf.predict(test_x);
-print("Predict:", pred[0] );
-print("Truth:", test_y[0]);
+# Split the data into training and testing sets
+train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.5, random_state=20)
 
-# find true fraud for prediction
-num = 0;
+# Initialize a Logistic Regression classifier
+clf = LogisticRegression()
+
+# Train the classifier on the training data
+clf.fit(train_x, train_y)
+
+# Print the accuracy score of the classifier on the testing data
+print("Score:", clf.score(test_x, test_y))
+
+# Make predictions on the testing data
+pred = clf.predict(test_x)
+
+# Print the first prediction and the corresponding true value
+print("Predict:", pred[0])
+print("Truth:", test_y[0])
+
+# Find the index of the first true fraud in the testing data
+num = 0
 for i in range(len(test_y)):
     if test_y[i] == 1:
-        num = i;
-        break;
-        
-print("Predict:", pred[num] );
-print("Truth:", test_y[num]);
+        num = i
+        break
+
+# Print the prediction and true value for the first true fraud
+print("Predict:", pred[num])
+print("Truth:", test_y[num])
